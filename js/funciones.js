4d88685797b91f3f612d927da1d7e4589497d67e
dulcemargo01 <124635592+dulcemargo01@users.noreply.github.com>
2023-05-23 10:24:06 -0600

    Update funciones.js

--- js/funciones.js
+++ js/funciones.js
@@ -13,142 +13,7 @@ for (var i = 10; i < dropdown.length; i++) {
     });
 }
 
-// Obtener los elementos HTML necesarios
 
-const cartItemsEl = document.getElementById("cart-items");
-const cartTotalEl = document.getElementById("cart-total");
-const productItems = document.querySelectorAll("#product-items li");
-
-// Variables para mantener el estado del carrito
-let cartItems = [];
-let cartTotal = 0;
-
-// Función para agregar un elemento al carrito
-function addToCart(name, price) {
-  // Verificar si el producto ya está en el carrito
-  for (let i = 0; i < cartItems.length; i++) {
-    if (cartItems[i].name === name) {
-      cartItems[i].quantity++;
-      cartItems[i].total = cartItems[i].quantity * price;
-      renderCart();
-      return;
-    }
-  }
-
-  // Si el producto no está en el carrito, agregarlo
-  cartItems.push({
-    name: name,
-    price: price,
-    quantity: 1,
-    total: price,
-  });
-
-  renderCart();
-}
-
-// Función para eliminar un elemento del carrito
-function removeFromCart(name) {
-  // Buscar el índice del elemento en el carrito
-  const index = cartItems.findIndex((item) => item.name === name);
-
-  if (index !== -1) {
-    // Si se encontró el elemento, eliminarlo del carrito
-    cartItems.splice(index, 1);
-    renderCart();
-  }
-}
-
-// Función para renderizar el carrito
-function renderCart() {
-  // Limpiar el contenido actual del carrito
-  cartItemsEl.innerHTML = "";
-  cartTotal = 0;
-
-  // Agregar cada elemento del carrito al HTML
-  for (let i = 0; i < cartItems.length; i++) {
-    const item = cartItems[i];
-
-    const li = document.createElement("li");
-    li.innerHTML = `${item.name} x ${item.quantity} = $${item.total.toFixed(
-      2
-    )} <button class="remove-from-cart">Eliminar</button>`;
-    cartItemsEl.appendChild(li);
-
-    cartTotal += item.total;
-  }
-
-  // Actualizar el total del carrito
-  cartTotalEl.innerHTML = `$${cartTotal.toFixed(2)}`;
-
-  // Agregar el evento "click" a los botones de "Eliminar"
-  const removeButtons = document.querySelectorAll(".remove-from-cart");
-  removeButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const name = button.parentElement.textContent.split(" x ")[0];
-      removeFromCart(name);
-    });
-  });
-}
-
-
-// Agregar el evento "click" a los botones de "Agregar"
-productItems.forEach((item) => {
-  const name = item.dataset.name;
-  const price = parseFloat(item.dataset.price);
-
-  item.querySelector(".add-to-cart").addEventListener("click", () => {
-    addToCart(name, price);
-  });
-});
-
-		const pagarButton = document.querySelector('.pagar .add-to-cart');
-pagarButton.addEventListener('click', handlePagar);
-
-function handlePagar() {
-  const paymentOptions = document.getElementsByName('payment-option');
-  let selectedOption;
-
-  for (let i = 0; i < paymentOptions.length; i++) {
-    if (paymentOptions[i].checked) {
-      selectedOption = paymentOptions[i].value;
-      break;
-    }
-  }
-
-  if (selectedOption) {
-    // Aquí puedes realizar acciones según la opción de pago seleccionada
-    switch (selectedOption) {
-      case 'tarjeta':
-        // Lógica para procesar el pago con tarjeta
-        break;
-      case 'efectivo':
-        // Lógica para procesar el pago en efectivo
-        break;
-      case 'transferencia':
-        // Lógica para procesar el pago por transferencia
-        break;
-    }
-
-    // Lógica adicional para completar la compra, enviar el correo electrónico, etc.
-  } else {
-    alert('Por favor, selecciona una opción de pago.');
-  }
-}
-
-$(".pagar").on("click",function()
-{$(".opc").show();
-
-alert('usted compro')
-
-});
-
-
-	
-		
-	$(".pagar").on("click",function()
-	{$(".opc").show();
-	
-	});
 
 	
 		$(".container").hide();
@@ -361,6 +226,13 @@ function handlePagar() {
     alert('Por favor, selecciona una opción de pago.');
   }
 }
+
+$(".pagar").on("click",function()
+{$(".opc").show();
+
+alert('usted compro')
+
+});
 
 
 	
